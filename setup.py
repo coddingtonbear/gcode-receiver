56ad1f75a337f23ed9c9733dddf1ca157ccb5cc7
Adam Coddington <me@adamcoddington.net>
2018-12-28 14:29:14 -0800

    Add install_requires directive.

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from setuptools import setup, find_packages
 
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+
 setup(
     name='gcode-receiver',
-    version='0.1',
+    version='1.0',
     description='A very-dumb Gcode receiver for an integration test.',
     url='https://github.com/coddingtonbeear/gcode-receiver',
     author='Adam Coddington',
     author_email='me@adamcoddington.net',
     licence='MIT',
     packages=find_packages(),
+    install_requires=required,
     entry_points={
         'console_scripts': [
             'gcode-receiver=gcode_receiver.cmdline:main'
